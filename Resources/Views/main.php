945eabe2d18cdf8c46112e6635022f6f3dca8e18
César Mejía <jucesarin26@gmail.com>
2019-03-12 20:57:29 -0600

    agregando bootstrap

--- Resources/Views/main.php
+++ Resources/Views/main.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<title>Principal</title>
 </head>
 <body style="background: linear-gradient(to right, rgba(0,255,0,0), rgba(0,255,200,1));">
 	<h1 style="color: #888;">LaraCode</h1>
 	<p><h4>Bienvenido</h4></p>
+	
+	<div class="container">
+		<h2>Basic Table</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Nombre</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>no.1</td>
+					<td>Cesar</td>
+				</tr>
+			</tbody>
+		</table>	
+	</div>
 </body>
 </html>
