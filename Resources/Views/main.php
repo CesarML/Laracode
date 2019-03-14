45586894ba7c09b61af6ff930df5220fda387440
César Mejía <jucesarin26@gmail.com>
2019-03-13 21:16:15 -0600

    Creando modal en bootstrap

--- Resources/Views/main.php
+++ Resources/Views/main.php
@@ -10,21 +10,29 @@
 	<p><h4>Bienvenido</h4></p>
 	
 	<div class="container">
-		<h2>Basic Table</h2>
-		<table class="table table-bordered table-striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Nombre</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>no.1</td>
-					<td>Cesar</td>
-				</tr>
-			</tbody>
-		</table>	
+		<!--trigger the modal with a button-->
+		<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Open Modal</button>
+		
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" role="dialog">
+			<div class="moal-dialog">
+				
+				<!-- Modal Content -->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-tittle">Modal Header</h4>
+					</div>
+					<div class="modal-body">
+						<p>Este es su nuevo proyecto creado en Laracode</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-defaul" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+
+			</div>
+		</div>
 	</div>
 </body>
 </html>
