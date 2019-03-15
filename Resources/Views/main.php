a27f373e08424bf0d1b3dad7b1e820124119e8c2
César Mejía <jucesarin26@gmail.com>
2019-03-15 12:42:11 -0600

    Ruta del CSS

--- Resources/Views/main.php
+++ Resources/Views/main.php
@@ -13,8 +13,8 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7
 	/js/bootstrap.min.js"></script>
 
-	<link rel="stylesheet" href="../CSS/normalize.css">
-	<link rel="stylesheet" href="../CSS/estilos.css">
+	<link rel="stylesheet" href="./Resources/CSS/normalize.css">
+	<link rel="stylesheet" href="./Resources/CSS/estilos.css">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 	<title>Principal</title>
